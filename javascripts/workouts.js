c103a591ef709734c8190d4cb77b2c0f964d04ef
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-02-06 10:52:12 -0500

    adding gyrojs, first attempt at mobile control

--- javascripts/workouts.js
+++ javascripts/workouts.js
@@ -1,4 +1,74 @@
 ;(function(){
-  
 
+  var camera, scene, renderer;
+  var mesh;
+
+  init();
+  animate();
+
+  function init() {
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    //
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth /
+      window.innerHeight, 1, 1000 );
+      camera.position.z = 600;
+
+      scene = new THREE.Scene();
+
+      var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+
+      var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
+      texture.anisotropy = renderer.getMaxAnisotropy();
+
+      var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+
+      //
+
+      window.addEventListener( 'resize', onWindowResize, false );
+
+    }
+
+    function initGyro() {
+      console.log("start tracking");
+      gyro.startTracking(function(o) {
+
+        // f.innerHTML = gyro.getFeatures();
+        // console.log(o.x.toFixed(3));
+        // console.log(o.y.toFixed(3));
+
+        mesh.position.x = o.x.toFixed(3);
+        mesh.position.y = o.y.toFixed(3);
+        renderer.render( scene, camera );
+        // o.z
+        // o.alpha
+        // o.beta
+        // o.gamma
+      });
+    }
+
+    function onWindowResize() {
+
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize( window.innerWidth, window.innerHeight );
+
+    }
+
+    function animate() {
+
+      requestAnimationFrame( animate );
+
+      renderer.render( scene, camera );
+
+    }
 })();
