3ad27227236b5e9a53e186848ac31ce1b8359e52
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-02-13 20:06:44 -0500

    some changes

--- javascripts/recordData.js
+++ javascripts/recordData.js
@@ -2,44 +2,45 @@
 // retrieving acceleration data
 window.onload = function() {
   var xA = document.getElementById("xA"),
-    inpLabel = document.getElementById("inpLabel"),
-    btnStartStop = document.getElementById("startStop"),
-    spnJson = document.getElementById("json");
+  inpLabel = document.getElementById("inpLabel"),
+  btnStartStop = document.getElementById("startStop"),
+  spnJson = document.getElementById("json");
 
   var lastVelocity = false,
-    lastPosition = 0,
-    timeStep,
-    lastT = new Date(),
-    label,
-    //  {
-    //    up: [
-    //      [ accl, accl ],
-    //      [ accl, accl ]
-    //    ],
-    //    down: [
-    //      [ accl, accl ],
-    //      [ accl, accl ]
-    //    ]
-    //  }
-    //
-    //  {
-    //    movements: [{
-    //      type: 'up',
-    //      list: [ accl, accl ]
-    //    },{
-    //      type: 'up',
-    //      list: [ accl, accl ]
-    //    },{
-    //      type: 'down',
-    //      list: [ accl, accl ]
-    //    },{
-    //      type: 'up',
-    //      list: [ accl, accl ]
-    //    }]
-    //  }
-    //
-    measurements = {},
-    values = [];
+  lastPosition = 0,
+  timeStep,
+  lastT = new Date(),
+  label, min, max,
+  accels = [],
+  //  {
+  //    up: [
+  //      [ accl, accl ],
+  //      [ accl, accl ]
+  //    ],
+  //    down: [
+  //      [ accl, accl ],
+  //      [ accl, accl ]
+  //    ]
+  //  }
+  //
+  //  {
+  //    movements: [{
+  //      type: 'up',
+  //      list: [ accl, accl ]
+  //    },{
+  //      type: 'up',
+  //      list: [ accl, accl ]
+  //    },{
+  //      type: 'down',
+  //      list: [ accl, accl ]
+  //    },{
+  //      type: 'up',
+  //      list: [ accl, accl ]
+  //    }]
+  //  }
+  //
+  measurements = {},
+  values = [];
 
 
   btnStartStop.onclick = function(e) {
@@ -68,29 +69,128 @@ window.onload = function() {
     gyro.frequency = 10;
   }
 
+  var threshold = 3,
+    numAccelsToAverage = 6;
+
+  var thresholdCount = 0;
+
+  function average (list) {
+    var avg = 0,
+      sum = 0;
+
+    for (var i = 0; i < list.length; i++) {
+      sum += parseFloat(list[i]);
+    }
+
+    return sum / list.length;
+  }
+
+  function getRollingAverage () {
+    var numAccels = accels.length,
+      sIndex = numAccels - numAccelsToAverage,
+      eIndex = numAccels - 1,
+      accelsInRange = accels.slice(sIndex, eIndex);
+
+    console.log(accelsInRange);
+
+    return average(accelsInRange);
+  }
+
+  var recording = false;
+
   function startTracking () {
     gyro.startTracking(function(o) {
-      var a = o.x.toFixed(5);
+      var a = parseFloat(o.x.toFixed(5));
 
       xA.innerHTML = a;
 
-      tracking = true;
+      accels.push(a);
 
-      values.push(a);
+      if (a < threshold || a > -threshold) {
+        thresholdCount++;
+
+      } else {
+        threshold = 0;
+      }
+
+      var rollingAverage = getRollingAverage();
+
+      if (!recording && rollingAverage > threshold ||
+          rollingAverage < -threshold) {
+
+        // motion started
+
+        // startRecording
+        recording = true;
+      }
+
+      if (recording) {
+        values.push(a);
+
+        if (a > max) {
+          max = a;
+
+        } else if (a < min) {
+          min = a;
+        }
+      }
+
+      if (thresholdCount > 30 && recording) {
+        var movement = new Movement(min, max, values);
+
+        // motion stopped
+        thresholdCount = 0;
+
+        recording = false;
+        min = 0;
+        max = 0;
+        values = [];
+        console.log(movement);
+        movement.normalize();
+        console.log(movement);
+        // normalize values
+        // if training save to measurements object
+        // if production -> send to NN
+      }
+
     });
   }
 
   function stopTracking () {
     gyro.stopTracking();
-    tracking = false;
 
     if (typeof measurements[label] == "undefined") {
       measurements[label] = [];
     }
+
     measurements[label].push(values);
     values = [];
     spnJson.innerHTML = JSON.stringify(measurements);
   }
+
   initGyro();
+
+  var Movement = function (min, max, list) {
+    this.min = min ? min : 0;
+    this.max = max ? max : 0;
+    this.list = list ? list : [];
+  }
+
+  Movement.prototype.normalize = function () {
+    this.max += Math.abs(this.min);
+
+    var range = this.max,
+      normalized = [],
+      a;
+
+    console.log(range);
+
+    for (var i = 0; i < this.list.length; i++) {
+      a = this.list[i] + Math.abs(this.min);
+      normalized[i] = a / range;
+    }
+
+    this.normalizedList = normalized;
+  }
 
 }
