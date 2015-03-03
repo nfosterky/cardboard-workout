371a3757c744c6da77049231e3f911d7c30c7c7d
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-03-02 23:01:56 -0500

    adding movement controls, still a bit iffy, might need to use velocity instead of exact position

--- javascripts/ephemeral.js
+++ javascripts/ephemeral.js
@@ -1,6 +1,6 @@
 // ephemeral.js
 // scene where glitchy controls thrive
-
+var QUARTER_CIRCLE_RADIANS = 1.57079633; // Math.PI / 2
 var SCENE_HEIGHT = 1000;
 
 // create scene, camera, one plane, light,
@@ -12,7 +12,7 @@ function init() {
   camera = new THREE.PerspectiveCamera( 45, window.innerWidth /
       window.innerHeight, 1, 2000 );
 
-	camera.position.set( 0, 200, 800 );
+	camera.position.set( 0, 0, 0 );
 
   deviceControls = new THREE.DeviceOrientationControls( camera );
 
@@ -38,7 +38,7 @@ function init() {
 	ground = new THREE.Mesh( new THREE.PlaneBufferGeometry( 10000, 10000 ),
       groundMaterial );
 
-	ground.position.y = 10;
+	ground.position.y = -10;
 	ground.rotation.x = -Math.PI / 2;
   camera.rotation.x = -Math.PI / 2;
 	ground.receiveShadow = true;
@@ -50,6 +50,7 @@ function init() {
   document.body.appendChild( renderer.domElement );
 
   startBalloons();
+  makeHUD();
 }
 
 function animate() {
@@ -89,14 +90,16 @@ var balloons = [];
  */
 function makeBalloon (pos) {
   // make sphere
-  var geometry = new THREE.SphereGeometry(15, 10, 10)
-  groundTexture = THREE.ImageUtils.loadTexture(
+  var geometry = new THREE.SphereGeometry(15, 10, 10);
+
+  var groundTexture = THREE.ImageUtils.loadTexture(
       "textures/moon.png" );
+
   // TODO: random color
   var material = new THREE.MeshPhongMaterial({
     color: 0xaff00,
     map: groundTexture
-  })
+  });
 
   var mesh = new THREE.Mesh( geometry, material );
 
@@ -113,7 +116,7 @@ function makeBalloon (pos) {
     z: pos.z
   };
 
-  doTween(pos, target, mesh, TWEEN.Easing.Cubic.In, 25000)
+  doTween(pos, target, mesh, TWEEN.Easing.Cubic.Out, 25000)
 }
 
 var lastBalloonReset = 0;
@@ -161,6 +164,92 @@ function doTween (position, target, obj, easing, time) {
   tween.start();
 }
 
+function findNewPoint (x, y, angleRadians, distance) {
+    var result = {};
+
+    result.x = Math.round(Math.cos(angleRadians) * distance + x);
+    result.z = Math.round(Math.sin(angleRadians) * distance + y);
+
+    return result;
+}
+
+function addKeyHandler () {
+  document.addEventListener( 'keyup', onKeyUp, false);
+}
+
+// TODO: start using velocities as demonstrated in example_3d_movement.html
+function onKeyUp (event) {
+  var increment = 50,
+      angleY    = camera.rotation.y,
+      x         = camera.position.x,
+      z         = camera.position.z,
+      distance;
+
+  event.preventDefault();
+
+  console.log(event.keyCode);
+
+  switch(event.keyCode) {
+    case 38: //Up
+    angleY += QUARTER_CIRCLE_RADIANS
+    distance = increment;
+    break;
+
+    case 40: //Down
+    angleY -= QUARTER_CIRCLE_RADIANS
+    distance = increment;
+    break;
+
+    case 39: //Right
+    distance = -increment;
+    break;
+
+    case 37:// left
+    distance = increment;
+    break;
+  }
+
+  var newPoint = findNewPoint(x, z, angleY, distance);
+
+  camera.position.x = newPoint.x;
+  camera.position.z = newPoint.z;
+}
+
+function makeHUD () {
+  var geometry = new THREE.SphereGeometry(15, 10, 10);
+
+  var texture = THREE.ImageUtils.loadTexture(
+      "textures/golfball.jpg" );
+
+  var material = new THREE.MeshPhongMaterial({
+    color: 0xaff00,
+    map: texture
+  });
+
+  var huds = [];
+
+  for (var i = 0; i < 6; i++) {
+    huds[i] = new THREE.Mesh( geometry, material );
+    camera.add(huds[i]);
+  }
+
+  var offset = 20;
+
+  huds[0].position.z =  offset;
+  huds[1].position.z = -offset;
+
+  huds[2].position.y =  offset;
+  huds[3].position.y = -offset;
+
+  huds[4].position.x =  offset;
+  huds[5].position.x = -offset;
+
+  // huds.lookAt(camera.position);
+
+  console.log("mesh", huds);
+  console.log("camera", camera);
+}
 
 init();
 animate();
+addKeyHandler();
